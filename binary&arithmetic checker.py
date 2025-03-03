eb92e9e9feb670039e1b37ed5a6fa1823ebeccad
Rachmat Ramadhan <153681663+rachmatramadhan123@users.noreply.github.com>
2025-03-03 20:19:53 +0700

    Update binary&arithmetic checker.py

--- binary&arithmetic checker.py
+++ binary&arithmetic checker.py
@@ -1,24 +1,23 @@
 import re
 
-def is_binary_string(s):
-
+def binarystring(s):
+  
     return all(char in '01' for char in s)
 
-def is_arithmetic_expression(s):
- 
+def arithmeticex(s):
+  
     pattern = re.compile(r'^[\d+\-*/\s\(\)]+$')
     return bool(pattern.match(s))
 
 def main():
-
-    binary_input = input("Masukkan string untuk memeriksa apakah itu bilangan biner: ")
-    if is_binary_string(binary_input):
+    binary_input = input("Masukkan string binary untuk validasi")
+    if binarystring(binary_input):
         print(f"'{binary_input}' adalah bilangan biner")
     else:
         print(f"'{binary_input}' bukan bilangan biner")
 
-    arithmetic_input = input("Masukkan string untuk memeriksa apakah itu ekspresi aritmatika yang valid: ")
-    if is_arithmetic_expression(arithmetic_input):
+    arithmetic_input = input("Masukkan string aritmatika untuk validasi ")
+    if arithmeticex(arithmetic_input):
         print(f"'{arithmetic_input}' adalah ekspresi aritmatika")
     else:
         print(f"'{arithmetic_input}' bukan ekspresi aritmatika")
